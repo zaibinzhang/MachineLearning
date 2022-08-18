4bf0b774740c22bacb36d9c00296280ae171bf72
zaibinzhang <zaibinzhang@qq.com>
2022-08-18 23:00:27 +0800

    准备测试

--- MachineLearning/TensorFlowKerasTest/Program.cs
+++ MachineLearning/TensorFlowKerasTest/Program.cs
@@ -1,35 +1,59 @@
-﻿using static Tensorflow.Binding;
-using static Tensorflow.KerasApi;
-using Tensorflow;
-using Tensorflow.Keras;
-using Tensorflow.Keras.Layers;
-using Tensorflow.Keras.Utils;
+﻿using Tensorflow;
+using Tensorflow.Keras.Engine;
 using Tensorflow.NumPy;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
 
-int batch_size = 128;
-int num_classes = 10;
-int epochs = 12;
+var model = keras.Sequential();
 
-// input image dimensions
-int img_rows = 28, img_cols = 28;
-
-Shape input_shape = null;
-
-var ((x_train, y_train), (x_test, y_test)) = keras.datasets.mnist.load_data();
+#region 官方例子
 
 
-if (keras.backend.image_data_format() == ImageDataFormat.channels_first)
-{
-    x_train = x_train.reshape((x_train.shape[0], 1, img_rows, img_cols));
-    x_test = x_test.reshape((x_test.shape[0], 1, img_rows, img_cols));
-    input_shape = (1, img_rows, img_cols);
-}
-else
-{
-    x_train = x_train.reshape((x_train.shape[0], img_rows, img_cols, 1));
-    x_test = x_test.reshape((x_test.shape[0], img_rows, img_cols, 1));
-    input_shape = (img_rows, img_cols, 1);
-}
 
+
+//Model model;
+//NDArray x_train, y_train, x_test, y_test;
+
+////tf.enable_eager_execution();
+////PrepareData
+//(x_train, y_train, x_test, y_test) = keras.datasets.mnist.load_data();
+//x_train = x_train.reshape((60000, 784)) / 255f;
+//x_test = x_test.reshape((10000, 784)) / 255f;
+
+////BuildModel
+//// input layer
+//var inputs = keras.Input(shape: 784);
+
+//// 1st dense layer
+//var outputs = keras.layers.Dense(64, activation: keras.activations.Relu).Apply(inputs);
+
+//// 2nd dense layer
+//outputs = keras.layers.Dense(64, activation: keras.activations.Relu).Apply(outputs);
+
+//// output layer
+//outputs = keras.layers.Dense(10).Apply(outputs);
+
+//// build keras model
+//model = keras.Model(inputs, outputs, name: "mnist_model");
+//// show model summary
+//model.summary();
+
+//// compile keras model into tensorflow's static graph
+//model.compile(loss: keras.losses.SparseCategoricalCrossentropy(from_logits: true),
+//    optimizer: keras.optimizers.RMSprop(),
+//    metrics: new[] { "accuracy" });
+
+//// train model by feeding data and labels.
+//model.fit(x_train, y_train, batch_size: 64, epochs: 2, validation_split: 0.2f);
+
+//// evluate the model
+//model.evaluate(x_test, y_test, verbose: 2);
+
+//// save and serialize model
+//model.save("d:\\mnist_model.mod");
+
+// recreate the exact same model purely from the file:
+// model = keras.models.load_model("path_to_my_model");
+#endregion
 
 Console.ReadKey();
