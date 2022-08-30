84ec6a49a4deefed2e1d919db3771019becb36e0
张小狼 <zaibinzhang@qq.com>
2022-08-30 18:02:02 +0800

    1

--- MachineLearning/TensorFlowKerasTest/Program.cs
+++ MachineLearning/TensorFlowKerasTest/Program.cs
@@ -8,6 +8,13 @@ using Tensorflow.NumPy;
 using static Tensorflow.Binding;
 using static Tensorflow.KerasApi;
 
+/*
+参考网站
+https://keras.io/examples/vision/mnist_convnet/
+
+https://github.com/SciSharp/Keras.NET
+
+*/
 var numClasses = 10;
 var inputShape = new Shape(28, 28, 1);
 
@@ -28,11 +35,13 @@ var model = keras.Sequential();
 model.add(keras.Input(inputShape));
 model.add(keras.layers.Conv2D(32, kernel_size: (3, 3), activation: keras.activations.Relu));
 model.add(keras.layers.MaxPooling2D((2, 2)));
-model.add(keras.layers.Conv2D(32, kernel_size: (3, 3), activation: keras.activations.Relu));
+model.add(keras.layers.Conv2D(64, kernel_size: (3, 3), activation: keras.activations.Relu));
 model.add(keras.layers.MaxPooling2D((2, 2)));
 model.add(keras.layers.Flatten());
 model.add(keras.layers.Dropout(0.5f));
 model.add(keras.layers.Dense(numClasses, activation: keras.activations.Softmax));
+model.compile(loss: keras.losses.CategoricalCrossentropy(), optimizer: keras.optimizers.Adam(), metrics: new[] { "accuracy" });
+
 model.summary();
 
 Console.ReadKey();
